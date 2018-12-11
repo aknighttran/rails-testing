30058eefea6707dcda5e448d3113265123b259b0
Allison Knight-Tran <aknighttran@gmail.com>
2018-12-11 16:06:51 -0700

    finished testing for school

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,7 +2,8 @@ class School < ApplicationRecord
   # Validations
   validates_presence_of :name, :address, :principal, :capacity, :private_school
   validates_uniqueness_of :name, case_sensitive: true
-  
+  validates :capacity, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 52000}
+
   # Associations
   has_many :students, dependent: :destroy
 end
