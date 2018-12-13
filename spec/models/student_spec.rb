00c9c4a05a1ff23f08bde745f631e93c0b7188ea
Seol Kim <oksusu123@gmail.com>
2018-12-13 14:43:19 -0700

    model added for school

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,8 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Student, type: :model do
   
+  describe "before action" do
+    before :all do
+      @school = FactoryBot.create(:school)
+      @student = FactoryBot.create(:student, school_id: @school.id)
+    end
+    
+    describe "validations" do
+      it { should validate_presence_of :student_number }
+      it { should validate_presence_of :name }
+      it { should validate_presence_of :gpa }
+    end
+    it 'has a student number lower bound' do
+      should validate_numericality_of(:student_number).
+      is_greater_than_or_equal_to(0)
+    end
 
-
+    it 'as a student number higer bound' do
+      should validate_numericality_of(:student_number).
+      is_less_than_or_equal_to(10000)
+    end
   
+    describe "uniqueness" do
+      it { should validate_uniqueness_of(:name) }
+    end
 
+    describe "association" do
+      it { should belong_to(:school) }
+    end
+
+  end
 end 
